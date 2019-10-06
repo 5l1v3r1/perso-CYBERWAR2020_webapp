9ad2458983632e8682575ef1c2e67b8e752b5d20
pcq <pcq@localhost.localdomain>
2019-10-06 23:03:01 +0200

    Add all the apps

--- App/Web/DjangoServer/CYBERWAR2020/CYBERWAR2020/settings.py
+++ App/Web/DjangoServer/CYBERWAR2020/CYBERWAR2020/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'GameS.apps.GameSConfig',
     'login.apps.LoginConfig',
+    'game.apps.GameConfig',
+    'saloon.apps.SaloonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
